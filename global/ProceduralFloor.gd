91c0e0832cb25c750b1c6302f9f9fd148e5a8abf
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2024-03-24 18:29:39 -0400

    Initial commit

---
+++ global/ProceduralFloor.gd
@@ -0,0 +1,22 @@
+class_name ProceduralFloor extends Sprite2D
+
+@export var tile_size := Vector2(64, 64)
+@export var parallax_divisor: float = 1
+
+func _ready() -> void:
+	region_enabled = true
+	scale = tile_size / texture.get_size()
+
+func _process(delta: float) -> void:
+	var cam: Camera2D = get_viewport().get_camera_2d()
+	
+	if (not cam): return print("No camera found for floor rendering")
+	
+	var next_frame_zoom := cam.zoom
+	if (refs.player and cam == refs.player.get_node("Camera2D")): next_frame_zoom = refs.player.next_frame_zoom(delta)
+	
+	var cam_pos := cam.get_screen_center_position()
+	
+	global_position = cam_pos
+	region_rect.size = get_viewport_rect().size / next_frame_zoom / scale
+	region_rect.position = cam_pos / scale / parallax_divisor - region_rect.size / 2
---
+++ scenes/player/Player.gd
@@ -0,0 +1,32 @@
+class_name Player extends CharacterBody2D
+
+@export var movement_speed: int
+@export_range(0, 1) var rotation_smoothing: float
+@export var zoom_ratio: float
+@export_range(0, 1) var zoom_smoothing: float
+
+@onready var goal_zoom := float($Camera2D.zoom.x)
+
+func next_frame_zoom(delta: float) -> Vector2:
+	var res := lerpf($Camera2D.zoom.x, goal_zoom, 1 - (1 - zoom_smoothing) ** (delta * 60))
+	
+	return Vector2(res, res)
+
+func _physics_process(delta: float) -> void:
+	velocity = Input.get_vector("left", "right", "up", "down") * movement_speed
+	move_and_slide()
+
+func _process(delta: float) -> void:
+	rotation = lerp_angle(rotation, global_position.angle_to_point(get_global_mouse_position()), 1 - rotation_smoothing)
+	
+	$Camera2D.zoom = next_frame_zoom(delta)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("zoomin"):
+		goal_zoom *= zoom_ratio
+	
+	if Input.is_action_pressed("zoomout"):
+		goal_zoom /= zoom_ratio
+
+func _ready() -> void:
+	refs.player = self
